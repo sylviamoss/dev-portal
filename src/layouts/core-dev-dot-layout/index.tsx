371b361625501905cf23e4d4919763f5c0e2a51a
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-11-08 13:38:37 -0500

    Rendering AuthenticationErrorDialog

--- src/layouts/core-dev-dot-layout/index.tsx
+++ src/layouts/core-dev-dot-layout/index.tsx
@@ -4,13 +4,13 @@ import { useRouter } from 'next/router'
 import { DatadogHeadTag, DatadogScriptTag } from 'lib/datadog'
 import { makeWelcomeToast } from 'lib/make-welcome-notification'
 import { MobileMenuProvider } from 'contexts'
+import AuthenticationErrorDialog from 'components/authentication-error-dialog'
 import TabProvider from 'components/tabs/provider'
 import { CoreDevDotLayoutProps } from './types'
 import s from './core-dev-dot-layout.module.css'
 
 const CoreDevDotLayout = ({ children }: CoreDevDotLayoutProps) => {
-	const router = useRouter()
-	const { asPath, pathname, isReady } = router
+	const { asPath, pathname, isReady } = useRouter()
 
 	const isSwingset = asPath.startsWith('/swingset')
 	const isToastPath = pathname !== '/' && pathname !== '/_error' && !isSwingset
@@ -22,15 +22,18 @@ const CoreDevDotLayout = ({ children }: CoreDevDotLayoutProps) => {
 	}, [isReady, isToastPath])
 
 	return (
-		<MobileMenuProvider>
-			<TabProvider>
-				<Head>
-					<DatadogHeadTag />
-				</Head>
-				<div className={s.root}>{children}</div>
-				{isSwingset ? null : <DatadogScriptTag />}
-			</TabProvider>
-		</MobileMenuProvider>
+		<>
+			<MobileMenuProvider>
+				<TabProvider>
+					<Head>
+						<DatadogHeadTag />
+					</Head>
+					<div className={s.root}>{children}</div>
+					{isSwingset ? null : <DatadogScriptTag />}
+				</TabProvider>
+			</MobileMenuProvider>
+			<AuthenticationErrorDialog />
+		</>
 	)
 }
 
