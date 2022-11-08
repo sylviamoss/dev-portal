a61bfeee8056dd64ae3bff5ce85deb15b87aa32d
Ashlee Boyer <ashlee.boyer@hashicorp.com>
2022-11-08 13:43:40 -0500

    Rendering UserDropdownDebuggingItems

--- src/components/user-dropdown-disclosure/index.tsx
+++ src/components/user-dropdown-disclosure/index.tsx
@@ -1,55 +1,16 @@
+import { useFlags } from 'flags/client'
+import { getUserMeta } from 'lib/auth/user'
 import DropdownDisclosure, {
-	DropdownDisclosureButtonItem,
 	DropdownDisclosureDescriptionItem,
 	DropdownDisclosureLabelItem,
-	DropdownDisclosureLinkItem,
 	DropdownDisclosureSeparatorItem,
 } from 'components/dropdown-disclosure'
-import { getUserMeta } from 'lib/auth/user'
-import isAbsoluteUrl from 'lib/is-absolute-url'
 import {
 	UserDropdownDisclosureItem,
 	UserDropdownDisclosureProps,
 } from './types'
-
-/**
- * Handles rendering either a link item or a button item based on whether the
- * `href` or `onClick` property is given.
- */
-const renderItem = (
-	{ href, icon, label, onClick }: UserDropdownDisclosureItem,
-	itemIndex: number
-) => {
-	if (href) {
-		const isExternal = isAbsoluteUrl(href)
-		const rel = isExternal ? 'noreferrer noopener' : undefined
-		const target = isExternal ? '_blank' : undefined
-
-		return (
-			<DropdownDisclosureLinkItem
-				key={itemIndex}
-				href={href}
-				icon={icon}
-				rel={rel}
-				target={target}
-			>
-				{label}
-			</DropdownDisclosureLinkItem>
-		)
-	}
-
-	if (onClick) {
-		return (
-			<DropdownDisclosureButtonItem
-				key={itemIndex}
-				icon={icon}
-				onClick={onClick}
-			>
-				{label}
-			</DropdownDisclosureButtonItem>
-		)
-	}
-}
+import { renderItem } from './helpers'
+import { UserDropdownDebuggingItems } from './components'
 
 /**
  * A DropdownDisclosure intended to be used as a menu with actions/links for
@@ -62,6 +23,8 @@ const UserDropdownDisclosure = ({
 	user,
 }: UserDropdownDisclosureProps) => {
 	const { icon, label, description } = getUserMeta(user)
+	const { flags } = useFlags()
+	const enableDebugging = !!flags?.showDebugUserMenuItems
 
 	return (
 		<DropdownDisclosure
@@ -76,6 +39,7 @@ const UserDropdownDisclosure = ({
 			</DropdownDisclosureDescriptionItem>
 			<DropdownDisclosureSeparatorItem />
 			{items.map(renderItem)}
+			{enableDebugging ? <UserDropdownDebuggingItems /> : null}
 		</DropdownDisclosure>
 	)
 }
